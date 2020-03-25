1ac09ce07a466917bea0e4dab19ee92b3b29be28
Júnior <junior07moreno@gmail.com>
2020-03-25 13:29:22 -0300

    Front-end do app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+
+// JSX - HTML integrado dentro do JS
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
