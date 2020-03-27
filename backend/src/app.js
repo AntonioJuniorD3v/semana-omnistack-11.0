31907dd73945e029a6afe898475401756189e079
Júnior <junior07moreno@gmail.com>
2020-03-27 12:35:02 -0300

    Funcionalidades Avançadas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require('cors');
 const routes = require('./routes');
+const { errors } = require('celebrate');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json()); // Converter JSON em Objeto JS
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
